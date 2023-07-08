b16dd69013de2f095f2b8f10030598d1435594df
Rithick69 <rithickcy19@gmail.com>
2023-07-09 00:27:46 +0530

    Added Redux-Persist library to persist the state values after reload and save in localStorage

--- src/store.js
+++ src/store.js
@@ -1,16 +1,29 @@
 // creating store.
 import { legacy_createStore } from "redux";
 
+import { persistStore, persistReducer } from "redux-persist";
+
+import storage from 'redux-persist/lib/storage';
+
 // Adding the actions and RootReducer into store to start performing the action.
 
 import rootReducer from "./reducers/index";
 
+const persistConfig = {
+	key: 'root',
+	storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
 const store = legacy_createStore(
-	rootReducer,
+	persistedReducer,
 	/* preloadedState,*/
 	/* Adding Redux Extension to track store, through redux console. */
 	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
+
+export const persistor = persistStore(store);
 
 export default store;
 
