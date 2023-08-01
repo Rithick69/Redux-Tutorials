726f3e26d2cfbff6e17dca58b94aaf3674715a04
Rithick69 <rithickcy19@gmail.com>
2023-08-02 00:23:33 +0530

    Added Services and Contact components to Home page and also designed a custom scrollbar for webkit browsers.

--- src/components/portfolio_website/Home.jsx
+++ src/components/portfolio_website/Home.jsx
@@ -2,6 +2,8 @@ import React, { useEffect } from 'react';
 import HeroSection from './reusables/HeroSection';
 import { useDispatch } from 'react-redux';
 import { homeHero } from '../../actions';
+import Services from './Services';
+import Contact from './Contact';
 
 const Home = () => {
 	const dispatch = useDispatch();
@@ -14,7 +16,13 @@ const Home = () => {
 		);
 	}, [dispatch]);
 
-	return <HeroSection />;
+	return (
+		<>
+			<HeroSection />
+			<Services />
+			<Contact />
+		</>
+	);
 };
 
 export default Home;
